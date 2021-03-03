d94fb01922d8fdb1f96ad8d20ac06d3f44d289ef
Nathan Allary <n.allary@hotmail.com>
2021-03-03 14:27:12 +0100

    New modif 2

--- CCP1.js
+++ CCP1.js
@@ -1,5 +1,19 @@
 $(window).ready(function () {
 
-    $(#con)
+    $("#con").click(function (event) {
+        event.preventDefault()
+        $("#Inscription").hide()
+        $("#Connection").show()
+
+        }
+    )
+
+    $("#Ins").click(function (event) {
+            event.preventDefault()
+            $("#Connection").hide()
+            $("#Inscription").show()
+
+        }
+    )
 
 })
