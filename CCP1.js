7c15ea1e702c2ee47cd994ceea3c48bb5892aecc
Nathan Allary <n.allary@hotmail.com>
2021-03-05 10:12:31 +0100

    modif 10

--- CCP1.js
+++ CCP1.js
@@ -63,12 +63,12 @@ $(window).ready(() => {
 
     $("#RegisterForm").submit((event) => {
         event.preventDefault()
-        if (pseudo2 === "" || pseudo2.length < 4) {
-            alert ("pseudo incomplet")
+        if (pseudo2 == "" || pseudo2.length < 4) {
+            alert("pseudo incomplet")
         } else if (email2 == "") {
-            alert ("email incomplet")
-        } else if ((mdp2 == "" || mdp2.length < 8) && mdp2 == validmdp){
-            alert ("mdp incomplet")
+            alert("email incomplet")
+        } else if ((mdp2 == "" || mdp2.length < 8) && mdp2 == validmdp) {
+            alert("mdp incomplet")
         } else {
             alert("good days")
             let PseudoExist = false
@@ -76,7 +76,7 @@ $(window).ready(() => {
 
             let x
             for (x in locallogin.users) {
-                var actualuser =locallogin.users[x]
+                var actualuser = locallogin.users[x]
                 if (actualuser.pseudo == pseudo2) {
                     PseudoExist = true
                     alert("pseudo already exist")
@@ -107,5 +107,45 @@ $(window).ready(() => {
         }
 
     })
+
+    $("#LoginForm").submit((event) => {
+        event.preventDefault()
+        let PseudoExist2 = true
+        let EmailExist2 = true
+        let MdpExist2 = true
+
+        let y
+        for (y in locallogin.users) {
+            var actualuser2 = locallogin.users[y]
+            if (actualuser2.pseudo == pseudo) {
+                PseudoExist2 = false
+                alert("pseudo dont exist")
+                break;
+            }
+            if (actualuser2.email == pseudo) {
+                EmailExist2 = false
+                alert("email dont exist")
+                break;
+            }
+            if (actualuser2.mdp == mdp) {
+                MdpExist2 = false
+                alert("Mdp dont exist")
+                break;
+            }
+        }
+
+        if (PseudoExist2 == true && EmailExist2 == true && MdpExist2 == true) {
+            var id = id
+            var autoconnect = {
+                connect: true
+                id: id
+            }
+            locallogin.user.push(autoconnect)
+            sessionStorage.setItem("user", JSON.stringify(locallogin))
+        }
+        $("#pseudo").val("")
+        $("#mdp").val("")
+    })
+
 
 })
